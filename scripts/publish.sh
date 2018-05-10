d2cb60b6151e464ef698124f21d3b64c453e69e6
Julien Eluard <julien.eluard@gmail.com>
2018-05-10 12:00:55 +0200

    Fixed missing assets

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
-mkdir -p demo
+mkdir -p demo/assets
 clj -R:repl build.clj compile once
-cp resources/public/assets/pluto.js demo/
+cp resources/public/assets/*.js demo/assets
 cp resources/public/index.html demo/
