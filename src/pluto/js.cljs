229393d91ef80a6c43761ba358bec36d93641b25
jeluard <julien.eluard@gmail.com>
2018-10-04 13:00:17 +0200

    Added basic capacities. Improved QR code display.

--- src/pluto/js.cljs
+++ src/pluto/js.cljs
@@ -9,5 +9,16 @@
 (defn ^:export from-clj [o]
   (clj->js o))
 
-(defn ^:export parse [opts m]
-  (reader/parse (js->clj opts) (:data m)))
+(def ctx
+  {:capacities {:components {'text :text 'view :div 'status/token-selector :div 'status/asset-selector :div}
+                :hooks {:commands {:properties {:scope         #{:personal-chats :public-chats}
+                                                :description   :string
+                                                :short-preview :view
+                                                :preview       :view
+                                                :parameters    [{:id           :keyword
+                                                                  :type         {:one-of #{:text :phone :password :number}}
+                                                                  :placeholder  :string
+                                                                  :suggestions? :component}]}}}}})
+
+(defn ^:export parse [m]
+  (reader/parse ctx (:data m)))
