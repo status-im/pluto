414ca052f2b71884c198d627e1cd08ee9f2fef2b
Julien Eluard <julien.eluard@gmail.com>
2018-10-19 09:29:58 +0200

    Release iteration#3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject status-im/pluto "iteration-3-SNAPSHOT"
+(defproject status-im/pluto "iteration-3"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [org.clojure/tools.reader "1.2.1"]
