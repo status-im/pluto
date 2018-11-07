c1349d8c7bca70a3e66f1f66605e2e528356d281
Julien Eluard <julien.eluard@gmail.com>
2018-11-07 16:34:07 +0100

    Improved store error support

--- src/pluto/storage/http.cljs
+++ src/pluto/storage/http.cljs
@@ -2,10 +2,11 @@
   (:require [pluto.storage :as storage]))
 
 (defn result [xhr]
-  (let [status (.-status xhr)]
-    (if (= 404 status)
-      {:type :error :value status}
-      {:type :success :value [{:content (.-responseText xhr)}]})))
+  (let [status  (.-status xhr)
+        content (.-responseText xhr) ]
+    (if (= 200 status)
+      {:type :success :value {:content content}}
+      {:type :error :value {:status status :content content}})))
 
 ;; TODO Handle all edn files types, not only extension.edn
 
@@ -13,6 +14,7 @@
   storage/Storage
   (fetch [_ {:keys [value]} callback]
     (let [xhr (js/XMLHttpRequest.)]
+      (set! (.-timeout xhr) 5000)
       (.open xhr "GET" (str value "/extension.edn") true)
       (.send xhr nil)
       (set! (.-onreadystatechange xhr)
--- src/pluto/storage/ipfs.cljs
+++ src/pluto/storage/ipfs.cljs
@@ -3,10 +3,11 @@
             [pluto.storage  :as storage]))
 
 (defn result [xhr]
-  (let [status (.-status xhr)]
-    (if (= 404 status)
-      {:type :error :value status}
-      {:type :success :value [{:content (.-responseText xhr)}]})))
+  (let [status  (.-status xhr)
+        content (.-responseText xhr) ]
+       (if (= 200 status)
+         {:type :success :value {:content content}}
+         {:type :error :value {:status status :content content}})))
 
 ;; TODO Handle all edn files types, not only extension.edn
 
@@ -17,6 +18,7 @@
   storage/Storage
   (fetch [_ {:keys [value]} callback]
     (let [xhr (js/XMLHttpRequest.)]
+      (set! (.-timeout xhr) 5000)
       (.open xhr "GET" (infura-url value) true)
       (.send xhr nil)
       (set! (.-onreadystatechange xhr)
