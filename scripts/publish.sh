a37e83febffe4d90e362396832789e6235075d54
Julien Eluard <julien.eluard@gmail.com>
2019-01-11 10:53:42 +0100

    [Fixes #125] Removed now irrelevant example page

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,4 @@
 clojure -m cljs.main -O advanced -d website/static/js -o website/static/js/pluto.js -c pluto.js
-cp examples/resources/public/index.html website/pages/examples.html
 cd website
 GIT_USER=jeluard\
 CURRENT_BRANCH=#2 \
