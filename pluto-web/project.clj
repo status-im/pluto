ffbd00ed06b2cd36d3bf9ec6b0c4eca872c3e176
Julien Eluard <julien.eluard@gmail.com>
2019-04-01 13:09:41 +0200

    Removed older pluto-web

--- pluto-web/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject status-im/pluto-web "0.1.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]
-                 [org.clojure/tools.reader "1.2.1"]
-                 [reagent "0.8.0"]
-                 [re-frame "0.10.5"]
-                 [binaryage/devtools "0.9.10"]
-                 [day8.re-frame/re-frame-10x "0.3.2"]]
-  :source-paths ["src"])
